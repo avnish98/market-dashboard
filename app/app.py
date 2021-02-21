de1314edea0f4ffa4659f315552ac4a2739bfe43
bovem <avnishnish07@gmail.com>
2021-02-21 21:30:03 +0530

    Made temp home page

--- app/app.py
+++ app/app.py
@@ -7,13 +7,26 @@ import dash_html_components as html
 import dash_core_components as dcc
 
 from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import math
 
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/litera/bootstrap.min.css',
                         'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+metadata_df = pd.read_csv('nifty_500_metadata.csv')
+metadict = metadata_df[['symbol', 'companyName']].to_dict(orient='records')
+val_dict = [{'label':item['companyName'], 'value':item['symbol']} for item in metadict]
+
 app.layout = html.Div([
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=val_dict,
+        value='3MINDIA'
+    ),
     dcc.Tabs(id='tabs-example', value='tab-1', children=[
         dcc.Tab(label='Stock', value='tab-1'),
         dcc.Tab(label='OHLC', value='tab-2'),
@@ -24,12 +37,52 @@ app.layout = html.Div([
     html.Div(id='tabs-example-content')
 ])
 
+df = px.data.iris()
+df2 = pd.read_csv('3MINDIA.csv')
+
+# <div class="card border-secondary mb-3" style="max-width: 20rem;">
+#   <div class="card-header">Header</div>
+#   <div class="card-body">
+#     <h4 class="card-title">Secondary card title</h4>
+#     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+#   </div>
+# </div>
+
+def row_wrap(func):
+    return html.Div(style={'padding':'2rem'}, className='row', children=func)
+
+def card(title, value, text_color='text-white', bg_color='bg-primary'):
+    return html.Div(style={'max-width': '20rem', 'margin':'2rem'}, className="card {} {} mb-3".format(text_color, bg_color), 
+        children=[html.Div(className='card-header', children=title),
+        html.Div(className='card-body', children=[html.H4(className='card-title', children=value)])])
+
+def data_mapper(dump):
+    n_vals = 0
+    cards = []
+    for key, value in dump.items():
+        try:
+            if(math.isnan(value)):
+                continue
+            elif(value < 0):
+                cards.append(card(key, value, 'text-white', 'bg-danger'))
+            else:
+                cards.append(card(key, value)) 
+        except Exception as e:
+            print(e)
+    return row_wrap(cards)
+
+df3 = pd.read_csv("nifty_500_metadata.csv")
 @app.callback(Output('tabs-example-content', 'children'),
-              Input('tabs-example', 'value'))
-def render_content(tab):
+              Input('tabs-example', 'value'),
+              Input('demo-dropdown', 'value'))
+def render_content(tab, stock):
     if tab == 'tab-1':
-        return html.Div([
-            html.H3('Tab content 1')
+        return html.Div(className='jumbotron', children=[
+            #row_wrap(html.H1('3MINDIA')),
+            dcc.Graph(figure=px.scatter(pd.read_csv('static_files/NSE/{}.csv'.format(stock)), x='Date', y='Close')),
+            #html.Div(style={'padding':'2rem'}, className='row', children=[cards, cards, cards, cards, cards, cards]),
+            #row_wrap(card())
+            data_mapper(df3[df3['symbol'] == stock].to_dict(orient='records')[0])
         ])
     elif tab == 'tab-2':
         return html.Div([
---
+++ app/tabs.py
@@ -0,0 +1,5 @@
+from dash_html_components import html
+
+class Tab:
+    def __init__(self):
+        pass
